a37fb489f2f2a4cb9a8639633efe2336d562bde3
Wesley Godoy <w.g20@hotmail.com>
2021-10-27 09:50:40 -0300

    Created delete_fields() and Label warning for telephone

--- App.py
+++ App.py
@@ -23,34 +23,42 @@ class App:
         self.id = Entry(interface, width="5")
         self.id.place(x=520, y=40)
 
-        self.button_search = Button(interface, text="Buscar", command=self.search_user, width="10", background="#9932CC")
+        self.button_search = Button(interface, text="Buscar", command=self.search_user, width="10",
+                                    background="#9932CC")
         self.button_search.place(x=580, y=35)
 
-        self.text_name = Label(interface, text="Nome Completo:", font="Calibri", wraplength=500, justify=LEFT)
-        self.text_name.place(x=400, y=70)
+        self.text_name = Label(interface, text="*Nome Completo:", font="Calibri", justify=LEFT)
+        self.text_name.place(x=392, y=70)
 
         self.name = Entry(interface, width="40")
         self.name.place(x=520, y=70)
 
-        self.text_email = Label(interface, text="E-mail:", font="Calibri", wraplength=500, justify=LEFT)
-        self.text_email.place(x=400, y=100)
+        self.text_email = Label(interface, text="*E-mail:", font="Calibri", justify=LEFT)
+        self.text_email.place(x=392, y=100)
 
         self.email = Entry(interface, width="40")
         self.email.place(x=520, y=100)
 
-        self.text_tel = Label(interface, text="Telefone:", font="Calibri", wraplength=500, justify=LEFT)
-        self.text_tel.place(x=400, y=130)
+        self.text_tel = Label(interface, text="*Telefone:", font="Calibri", justify=LEFT)
+        self.text_tel.place(x=392, y=130)
 
         self.tel = Entry(interface, width="15")
         self.tel.place(x=520, y=130)
 
-        self.button_insert = Button(interface, text="Inserir", command=self.insert_user, width="8", background="#9932CC")
+        self.text_warning_tel = Label(interface, text="Insira apenas o DDD e número sem espaço")
+        self.text_warning_tel["font"] = ("Calibri", "8")
+        self.text_warning_tel.place(x=518, y=150)
+
+        self.button_insert = Button(interface, text="Inserir", command=self.insert_user, width="8",
+                                    background="#9932CC")
         self.button_insert.place(x=520, y=180)
 
-        self.button_update = Button(interface, text="Alterar", command=self.update_user, width="8", background="#9932CC")
+        self.button_update = Button(interface, text="Alterar", command=self.update_user, width="8",
+                                    background="#9932CC")
         self.button_update.place(x=586, y=180)
 
-        self.button_delete = Button(interface, text="Excluir", command=self.delete_user, width="8", background="#9932CC")
+        self.button_delete = Button(interface, text="Excluir", command=self.delete_user, width="8",
+                                    background="#9932CC")
         self.button_delete.place(x=652, y=180)
 
         self.answer = Label(interface)
@@ -64,7 +72,7 @@ class App:
         email_object = ValidateEmail(email)
 
         if email_object.validate_return() == False or tel_object.validate_return() == False:
-            self.answer["text"] = "E-mail ou telefone inválido"
+            self.answer["text"] = "E-mail e/ou telefone inválido"
         else:
 
             user = Users()
@@ -75,10 +83,7 @@ class App:
 
             self.answer["text"] = user.insert_user()
 
-            self.id.delete(0, END)
-            self.name.delete(0, END)
-            self.email.delete(0, END)
-            self.tel.delete(0, END)
+            self.delete_fields()
 
     def update_user(self):
         tel = self.tel.get()
@@ -90,7 +95,6 @@ class App:
         if email_object.validate_return() == False or tel_object.validate_return() == False:
             self.answer["text"] = "E-mail ou telefone inválido"
         else:
-
             user = Users()
 
             user.id_user = self.id.get()
@@ -100,10 +104,7 @@ class App:
 
             self.answer["text"] = user.update_user()
 
-            self.id.delete(0, END)
-            self.name.delete(0, END)
-            self.email.delete(0, END)
-            self.tel.delete(0, END)
+            self.delete_fields()
 
     def delete_user(self):
         user = Users()
@@ -112,10 +113,7 @@ class App:
 
         self.answer["text"] = user.delete_user()
 
-        self.id.delete(0, END)
-        self.name.delete(0, END)
-        self.email.delete(0, END)
-        self.tel.delete(0, END)
+        self.delete_fields()
 
     def search_user(self):
         user = Users()
@@ -124,17 +122,19 @@ class App:
 
         self.answer["text"] = user.select_user(id_user)
 
-        self.id.delete(0, END)
+        self.delete_fields()
+
         self.id.insert(INSERT, user.id_user)
-
-        self.name.delete(0, END)
         self.name.insert(INSERT, user.name)
-
-        self.email.delete(0, END)
         self.email.insert(INSERT, user.email)
-
-        self.tel.delete(0, END)
         self.tel.insert(INSERT, user.tel)
+
+    def delete_fields(self):
+        self.id.delete(0, END)
+        self.name.delete(0, END)
+        self.email.delete(0, END)
+        self.tel.delete(0, END)
+
 
 interface = Tk()
 interface.iconbitmap("ico_mm.ico")
